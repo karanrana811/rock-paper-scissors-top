59b5a7bd9a43a86416d951c0d85065960fd62a63
Karan Rana <19659594+karanrana811@users.noreply.github.com>
2024-01-04 22:15:55 +0530

    add basic ui and make game playable by user

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorsButton = document.querySelector('#scissors');
+const result = document.querySelector("#result");
+
+
 const getComputerChoice = () => {
     const randomNum = Math.floor(Math.random() * 3)
     if (randomNum === 0) {
@@ -8,25 +14,24 @@ const getComputerChoice = () => {
         return 'SCISSORS';
     }
 }
-
 const playGame = (playerChoice, computerChoice) => {
     console.log( computerChoice,  playerChoice)
     if (playerChoice === 'PAPER' && computerChoice === 'ROCK') {
         return 'Player wins'
     } else if (playerChoice === 'ROCK' && computerChoice === 'PAPER') {
         return 'Computer wins'
-    } else if (playerChoice === 'SCISSORS' || playerChoice === 'SCISSOR' && computerChoice === 'SCISSORS') {
+    } else if (playerChoice === 'SCISSORS' && computerChoice === 'SCISSORS') {
         return 'It\'s a draw'
     } else if (playerChoice === 'ROCK' && computerChoice === 'ROCK') {
         return 'It\'s a draw'
     } else if (playerChoice === 'PAPER' && computerChoice === 'PAPER') {
-        return 'it\'s a draw'
+        return 'It\'s a draw'
     } else if (playerChoice === 'ROCK' && computerChoice === 'SCISSORS') {
         return 'Player wins'
     } else if (playerChoice === 
-        'SCISSORS' || playerChoice === 'SCISSOR' && computerChoice === 'PAPER') {
+        'SCISSORS' && computerChoice === 'PAPER') {
             return 'Player wins'
-    } else if (playerChoice === 'SCISSORS' || playerChoice === 'SCISSOR' && computerChoice === 'ROCK') {
+    } else if (playerChoice === 'SCISSORS' && computerChoice === 'ROCK') {
         return 'Computer wins'
     } else if (playerChoice === 'PAPER' && computerChoice === 'SCISSORS') {
         return 'Computer wins'
@@ -35,28 +40,43 @@ const playGame = (playerChoice, computerChoice) => {
     }
 }
 
-const game = () => {
-    let computerWins = 0;
-    let playerWins = 0;
+rockButton.addEventListener('click', (e) => {
+    result.textContent = playGame(e.target.id.toUpperCase(), getComputerChoice());
+});
 
-    for (let i=0 ; i < 5 ; i++){
-        const result = playGame(prompt('What do you choose?').toUpperCase(), getComputerChoice());
-        if (result === 'Player wins'){
-            playerWins++;
-            console.log('Player wins this round')
-        } else if ( result === 'Computer wins') {
-            computerWins++;
-            console.log('Computer wins this round');
-        } else if ( result === 'Something went wrong!') {
-            i--;
-            console.log('Please provide a valid choice between rock, paper and scissors or scissor')
-        } else {
-            i--;
-            console.log('this round is a draw')
-        }
-    }
+paperButton.addEventListener('click', (e) => {
+    result.textContent = playGame(e.target.id.toUpperCase(), getComputerChoice());
+});
 
-    return playerWins > computerWins ? `Player wins` : 'Computer Wins';
-}
+scissorsButton.addEventListener('click', (e) => {
+    console.log(e.target.id)
+    result.textContent = playGame(e.target.id.toUpperCase(), getComputerChoice());
+});
 
-console.log(game());
+// console.log(playGame(prompt('What do you choose?').toUpperCase(), getComputerChoice()))
+
+// const game = () => {
+//     let computerWins = 0;
+//     let playerWins = 0;
+
+//     for (let i=0 ; i < 5 ; i++){
+//         const result = playGame(prompt('What do you choose?').toUpperCase(), getComputerChoice());
+//         if (result === 'Player wins'){
+//             playerWins++;
+//             console.log('Player wins this round')
+//         } else if ( result === 'Computer wins') {
+//             computerWins++;
+//             console.log('Computer wins this round');
+//         } else if ( result === 'Something went wrong!') {
+//             i--;
+//             console.log('Please provide a valid choice between rock, paper and scissors or scissor')
+//         } else {
+//             i--;
+//             console.log('this round is a draw')
+//         }
+//     }
+
+//     return playerWins > computerWins ? `Player wins` : 'Computer Wins';
+// }
+
+// console.log(game());
