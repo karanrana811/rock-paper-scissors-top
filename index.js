17979c76971000c52bfd45da07383d60ed9476f2
Karan Rana <19659594+karanrana811@users.noreply.github.com>
2024-01-06 22:06:50 +0530

    make the game playable for multiple rounds until one player emerges as winner by reaching score 5

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const rockButton = document.querySelector('#rock');
 const paperButton = document.querySelector('#paper');
 const scissorsButton = document.querySelector('#scissors');
-const result = document.querySelector("#result");
+const buttonContainer = document.querySelector('#buttonContainer');
+const score = document.querySelector('#score')
+const currRoundResult = document.querySelector("#result");
+const winner = document.querySelector("#winner")
 
+let computerWins = 0;
+let playerWins = 0;
+    
 
 const getComputerChoice = () => {
     const randomNum = Math.floor(Math.random() * 3)
@@ -40,43 +46,52 @@ const playGame = (playerChoice, computerChoice) => {
     }
 }
 
-rockButton.addEventListener('click', (e) => {
-    result.textContent = playGame(e.target.id.toUpperCase(), getComputerChoice());
-});
+const clickEventHandler = (event) => {
+    {
+        if(playerWins < 5 && computerWins < 5) {
+            let target = event.target;
+            switch (target.id) {
+                case 'rock':
+                    currRoundResult.textContent = playGame(target.id.toUpperCase(), getComputerChoice());
+                    break;
+                case 'scissors':
+                    currRoundResult.textContent = playGame(target.id.toUpperCase(), getComputerChoice());
 
-paperButton.addEventListener('click', (e) => {
-    result.textContent = playGame(e.target.id.toUpperCase(), getComputerChoice());
-});
+                    break;
+                case 'paper':
+                    currRoundResult.textContent = playGame(target.id.toUpperCase(), getComputerChoice());
 
-scissorsButton.addEventListener('click', (e) => {
-    console.log(e.target.id)
-    result.textContent = playGame(e.target.id.toUpperCase(), getComputerChoice());
-});
+                    break;
+                default:
+                    currRoundResult.textContent = 'Please click on a button!';
+                    break;
+            }
+            game();
+    } else {
+        playerWins === 5 ? winner.textContent = 'Player is the winner' : winner.textContent = 'Computer is the winner';
+        buttonContainer.removeEventListener('click', clickEventHandler)
+    }
+        
+}
+}
+    
+buttonContainer.addEventListener('click', clickEventHandler) 
+    
 
-// console.log(playGame(prompt('What do you choose?').toUpperCase(), getComputerChoice()))
 
-// const game = () => {
-//     let computerWins = 0;
-//     let playerWins = 0;
 
-//     for (let i=0 ; i < 5 ; i++){
-//         const result = playGame(prompt('What do you choose?').toUpperCase(), getComputerChoice());
-//         if (result === 'Player wins'){
-//             playerWins++;
-//             console.log('Player wins this round')
-//         } else if ( result === 'Computer wins') {
-//             computerWins++;
-//             console.log('Computer wins this round');
-//         } else if ( result === 'Something went wrong!') {
-//             i--;
-//             console.log('Please provide a valid choice between rock, paper and scissors or scissor')
-//         } else {
-//             i--;
-//             console.log('this round is a draw')
-//         }
-//     }
+const game = () => {
+    if (currRoundResult.textContent === 'Player wins'){
+        playerWins++;
+        console.log('Player wins this round')
+    } else if ( currRoundResult.textContent === 'Computer wins') {
+        computerWins++;
+        console.log('Computer wins this round');
+    } else if ( (currRoundResult.textContent === 'Something went wrong!') || (currRoundResult.textContent === 'Please click on a button!')) {
+        console.log('Please provide a valid choice between rock, paper and scissors or scissor')
+    } else {
+        console.log('this round is a draw')
+    }
+        score.textContent = `Score: Computer: ${computerWins}, Player: ${playerWins}`
+}
 
-//     return playerWins > computerWins ? `Player wins` : 'Computer Wins';
-// }
-
-// console.log(game());
